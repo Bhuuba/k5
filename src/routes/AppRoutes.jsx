460144bd0333435355c68fdea7abcbc6b9de1d46
Bhuuba <sparagovskiu@gmail.com>
2025-06-02 20:45:41 +0300

    refactor: move page components to pages directory

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import ProtectedRoute from "./ProtectedRoute";
 import PublicRoute from "./PublicRoute";
-import Profile from "../components/Profile/Profile";
-import Videoai from "../components/Video/Video";
-import Pdf from "../components/Pdf/Pdf";
+import Profile from "../pages/profile/Profile";
+import Videoai from "../pages/video/Video";
+import Pdf from "../pages/pdf/Pdf";
 import MyAccountPage from "../components/Authentication/components/account/AccountPage";
 import Login from "../components/Authentication/pages/LoginPage";
 import RegisterPage from "../components/Authentication/pages/RegisterPage";
-import Chat from "../components/Chat/Chat";
-import Prising from "../components/Prising/Prising";
+import Chat from "../pages/chat/Chat";
+import Prising from "../pages/pricing/Prising";
 import PaymentSuccess from "../components/Premium/components/PaymentSuccess/PaymentSuccess";
 
 const AppRoutes = () => (
